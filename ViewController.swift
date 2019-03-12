a77a187745fbbee9ac8d57a4f637da4c9170879f
Dennislaugh <35494856+Denislav17@users.noreply.github.com>
2019-03-12 13:12:02 +0000

    WeightViewController build
    Added first version of WeightViewController for CW

---
+++ ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  MobileAppCWDennis
+//
+//  Created by Denislav Rofetov on 12/03/2019.
+//  Copyright © 2019 Denislav Rofetov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btnWeight: UIBarButtonItem!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btnWeight_Clicked(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "Id_WeightPage", sender:self)
+    }
+    
+}
+
---
+++ Weight_ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  Weight_ViewController.swift
+//  MobileAppCWDennis
+//
+//  Created by Denislav Rofetov on 12/03/2019.
+//  Copyright © 2019 Denislav Rofetov. All rights reserved.
+//
+
+import UIKit
+
+class Weight_ViewController: UIViewController {
+
+    @IBOutlet weak var btnBack: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func btnBack_Clicked(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "Id1_HomePage", sender:self)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

daf9e4f4261d812ba8c87a4544d4de2eabb983b3
Denislav17 <dennislavroffetov@gmail.com>
2019-03-13 11:21:34 +0000

    Added keyboard to temp page

--- MobileAppCWDennis/MobileAppCWDennis/Temp_ViewController.swift
+++ MobileAppCWDennis/MobileAppCWDennis/Temp_ViewController.swift
@@ -31,7 +31,6 @@ class Temp_ViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
     @IBAction func txtCelsius_Selected(_ sender: Any) {
         txtCelsius.isSelected = true
         txtCelsius.backgroundColor = UIColor.lightGray
@@ -51,7 +50,6 @@ class Temp_ViewController: UIViewController {
         txtFahrenheit.text = "0"
         txtKelvin.text = "0"
     }
-    
     @IBAction func txtFahrenheit_Selected(_ sender: Any) {
         txtFahrenheit.isSelected = true
         txtFahrenheit.backgroundColor = UIColor.lightGray
@@ -71,8 +69,6 @@ class Temp_ViewController: UIViewController {
         txtFahrenheit.text = ""
         txtKelvin.text = "0"
     }
-    
-    
     @IBAction func txtKelvin_Selected(_ sender: Any) {
         txtKelvin.isSelected = true
         txtKelvin.backgroundColor = UIColor.lightGray
@@ -92,6 +88,392 @@ class Temp_ViewController: UIViewController {
         txtFahrenheit.text = "0"
         txtKelvin.text = ""
     }
+    
+    //Calculations
+    func CelsiusSelected(_ celsius: Double) -> Void {
+        txtFahrenheit.text = String(format:"%.2f", (celsius*(9/5) + 32))
+        txtKelvin.text = String(format:"%.2f", (celsius+273.15))
+    }
+    func FahrenheitSelected(_ fah: Double) -> Void {
+        txtCelsius.text = String(format:"%.2f", ((fah-32)*(5/9)))
+        txtKelvin.text = String(format:"%.2f", ((fah-32)*(5/9)+273.15))
+    }
+    func KelvinSelected(_ kelvin: Double) -> Void {
+        txtCelsius.text = String(format: "%.2f", (kelvin-273.15))
+        txtFahrenheit.text = String(format: "%.2f", (kelvin-273.15)*(9/5)+32)
+    }
+    //End of Calc
+    //Keyboard init
+    @IBAction func keyboardButton_Tapped(_ sender: UIButton) {
+        switch (sender.titleLabel?.text) {
+        case "1":
+            // 1
+            if(txtCelsius.isSelected)
+            {
+                if(txtCelsius.text == "0") {
+                    txtCelsius.text = "1"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                } else {
+                    txtCelsius.text = txtCelsius.text! + "1"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                }
+            }
+            if (txtFahrenheit.isSelected) {
+                if(txtFahrenheit.text == "0"){
+                    txtFahrenheit.text = "1"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                } else {
+                    txtFahrenheit.text = txtFahrenheit.text! + "1"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                }
+            }
+            if (txtKelvin.isSelected){
+                if(txtKelvin.text == "0"){
+                    txtKelvin.text = "1"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                } else {
+                    txtKelvin.text = txtKelvin.text! + "1"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                }
+            }
+        case "2":
+            // 2
+            if(txtCelsius.isSelected)
+            {
+                if(txtCelsius.text == "0") {
+                    txtCelsius.text = "2"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                } else {
+                    txtCelsius.text = txtCelsius.text! + "2"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                }
+            }
+            if (txtFahrenheit.isSelected) {
+                if(txtFahrenheit.text == "0"){
+                    txtFahrenheit.text = "2"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                } else {
+                    txtFahrenheit.text = txtFahrenheit.text! + "2"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                }
+            }
+            if (txtKelvin.isSelected){
+                if(txtKelvin.text == "0"){
+                    txtKelvin.text = "2"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                } else {
+                    txtKelvin.text = txtKelvin.text! + "2"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                }
+            }
+        case "3":
+            // 3
+            if(txtCelsius.isSelected)
+            {
+                if(txtCelsius.text == "0") {
+                    txtCelsius.text = "3"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                } else {
+                    txtCelsius.text = txtCelsius.text! + "3"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                }
+            }
+            if (txtFahrenheit.isSelected) {
+                if(txtFahrenheit.text == "0"){
+                    txtFahrenheit.text = "3"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                } else {
+                    txtFahrenheit.text = txtFahrenheit.text! + "3"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                }
+            }
+            if (txtKelvin.isSelected){
+                if(txtKelvin.text == "0"){
+                    txtKelvin.text = "3"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                } else {
+                    txtKelvin.text = txtKelvin.text! + "3"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                }
+            }
+        case "4":
+            // 4
+            if(txtCelsius.isSelected)
+            {
+                if(txtCelsius.text == "0") {
+                    txtCelsius.text = "4"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                } else {
+                    txtCelsius.text = txtCelsius.text! + "4"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                }
+            }
+            if (txtFahrenheit.isSelected) {
+                if(txtFahrenheit.text == "0"){
+                    txtFahrenheit.text = "4"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                } else {
+                    txtFahrenheit.text = txtFahrenheit.text! + "4"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                }
+            }
+            if (txtKelvin.isSelected){
+                if(txtKelvin.text == "0"){
+                    txtKelvin.text = "4"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                } else {
+                    txtKelvin.text = txtKelvin.text! + "4"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                }
+            }
+        case "5":
+            // 5
+            if(txtCelsius.isSelected)
+            {
+                if(txtCelsius.text == "0") {
+                    txtCelsius.text = "5"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                } else {
+                    txtCelsius.text = txtCelsius.text! + "5"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                }
+            }
+            if (txtFahrenheit.isSelected) {
+                if(txtFahrenheit.text == "0"){
+                    txtFahrenheit.text = "5"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                } else {
+                    txtFahrenheit.text = txtFahrenheit.text! + "5"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                }
+            }
+            if (txtKelvin.isSelected){
+                if(txtKelvin.text == "0"){
+                    txtKelvin.text = "5"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                } else {
+                    txtKelvin.text = txtKelvin.text! + "5"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                }
+            }
+        case "6":
+            // 6
+            if(txtCelsius.isSelected)
+            {
+                if(txtCelsius.text == "0") {
+                    txtCelsius.text = "6"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                } else {
+                    txtCelsius.text = txtCelsius.text! + "6"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                }
+            }
+            if (txtFahrenheit.isSelected) {
+                if(txtFahrenheit.text == "0"){
+                    txtFahrenheit.text = "6"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                } else {
+                    txtFahrenheit.text = txtFahrenheit.text! + "6"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                }
+            }
+            if (txtKelvin.isSelected){
+                if(txtKelvin.text == "0"){
+                    txtKelvin.text = "6"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                } else {
+                    txtKelvin.text = txtKelvin.text! + "6"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                }
+            }
+        case "7":
+            // 7
+            if(txtCelsius.isSelected)
+            {
+                if(txtCelsius.text == "0") {
+                    txtCelsius.text = "7"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                } else {
+                    txtCelsius.text = txtCelsius.text! + "7"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                }
+            }
+            if (txtFahrenheit.isSelected) {
+                if(txtFahrenheit.text == "0"){
+                    txtFahrenheit.text = "7"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                } else {
+                    txtFahrenheit.text = txtFahrenheit.text! + "7"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                }
+            }
+            if (txtKelvin.isSelected){
+                if(txtKelvin.text == "0"){
+                    txtKelvin.text = "7"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                } else {
+                    txtKelvin.text = txtKelvin.text! + "7"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                }
+            }
+        case "8":
+            // 8
+            if(txtCelsius.isSelected)
+            {
+                if(txtCelsius.text == "0") {
+                    txtCelsius.text = "8"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                } else {
+                    txtCelsius.text = txtCelsius.text! + "8"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                }
+            }
+            if (txtFahrenheit.isSelected) {
+                if(txtFahrenheit.text == "0"){
+                    txtFahrenheit.text = "8"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                } else {
+                    txtFahrenheit.text = txtFahrenheit.text! + "8"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                }
+            }
+            if (txtKelvin.isSelected){
+                if(txtKelvin.text == "0"){
+                    txtKelvin.text = "8"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                } else {
+                    txtKelvin.text = txtKelvin.text! + "8"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                }
+            }
+        case "9":
+            // 9
+            if(txtCelsius.isSelected)
+            {
+                if(txtCelsius.text == "0") {
+                    txtCelsius.text = "9"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                } else {
+                    txtCelsius.text = txtCelsius.text! + "9"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                }
+            }
+            if (txtFahrenheit.isSelected) {
+                if(txtFahrenheit.text == "0"){
+                    txtFahrenheit.text = "9"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                } else {
+                    txtFahrenheit.text = txtFahrenheit.text! + "9"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                }
+            }
+            if (txtKelvin.isSelected){
+                if(txtKelvin.text == "0"){
+                    txtKelvin.text = "9"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                } else {
+                    txtKelvin.text = txtKelvin.text! + "9"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                }
+            }
+        case "0":
+            // 0
+            if(txtCelsius.isSelected)
+            {
+                if(txtCelsius.text == "0") {
+                    txtCelsius.text = "0"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                } else {
+                    txtCelsius.text = txtCelsius.text! + "0"
+                    CelsiusSelected(Double(txtCelsius.text!)!)
+                }
+            }
+            if (txtFahrenheit.isSelected) {
+                if(txtFahrenheit.text == "0"){
+                    txtFahrenheit.text = "0"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                } else {
+                    txtFahrenheit.text = txtFahrenheit.text! + "0"
+                    FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                }
+            }
+            if (txtKelvin.isSelected){
+                if(txtKelvin.text == "0"){
+                    txtKelvin.text = "0"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                } else {
+                    txtKelvin.text = txtKelvin.text! + "0"
+                    KelvinSelected(Double(txtKelvin.text!)!)
+                }
+            }
+        case ".":
+            // .
+            if(txtCelsius.isSelected)
+            { if(txtCelsius.text?.contains("."))!
+            {
+                //lblError.text = "Sorry, you can't add '.' twice!"
+            } else {
+                txtCelsius.text = txtCelsius.text! + "."
+                CelsiusSelected(Double(txtCelsius.text!)!)
+                }
+            }
+            if(txtFahrenheit.isSelected)
+            { if(txtFahrenheit.text?.contains("."))!
+            {
+                //lblError.text = "Sorry, you can't add '.' twice!"
+            } else {
+                txtFahrenheit.text = txtFahrenheit.text! + "."
+                FahrenheitSelected(Double(txtFahrenheit.text!)!)
+                }
+            }
+            if(txtKelvin.isSelected)
+            { if(txtKelvin.text?.contains("."))!
+            {
+                //lblError.text = "Sorry, you can't add '.' twice!"
+            } else {
+                txtKelvin.text = txtKelvin.text! + "."
+                KelvinSelected(Double(txtKelvin.text!)!)
+                }
+            }
+        //
+        case "-": break
+            
+        case "DEL":
+            // DEL
+            if(txtCelsius.isSelected)
+            {
+                txtCelsius.text = String((txtCelsius.text?.dropLast())!)
+                if(txtCelsius.text == "")
+                {
+                    txtCelsius.text = "0"
+                }
+                CelsiusSelected(Double(txtCelsius.text!)!)
+            }
+            
+            if (txtFahrenheit.isSelected) {
+                txtFahrenheit.text = String((txtFahrenheit.text?.dropLast())!)
+                if(txtFahrenheit.text == "")
+                {
+                    txtFahrenheit.text = "0"
+                }
+                FahrenheitSelected(Double(txtFahrenheit.text!)!)
+            }
+            if (txtKelvin.isSelected){
+                txtKelvin.text = String((txtKelvin.text?.dropLast())!)
+                if(txtKelvin.text == "")
+                {
+                    txtKelvin.text = "0"
+                }
+                KelvinSelected(Double(txtKelvin.text!)!)
+            }
+            //
+            
+        default:
+            break;
+        }
+    }
+    //End of Keyboard init
+    
     
     
     
